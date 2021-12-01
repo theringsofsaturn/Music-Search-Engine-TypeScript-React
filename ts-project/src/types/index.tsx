f1da1967fddcdf9caccaf868e0a502e67d6b9934
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-01 17:04:50 +0100

    commit

--- ts-project/src/types/index.tsx
+++ ts-project/src/types/index.tsx
@@ -7,8 +7,6 @@ export interface ISearch {
     link: string;
     duration: number;
     rank: number;
-    explicit_lyrics: boolean;
-    explicit_content_lyrics: number;
     explicit_content_cover: number;
     preview: string;
     md5_image: string;
